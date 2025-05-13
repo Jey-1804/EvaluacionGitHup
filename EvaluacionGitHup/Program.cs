f8d868b45bad7343a03a7de131666b81a3cf0e71
Jey-1818 <jeymih757@gmail.com>
2025-05-13 14:25:13 -0600

    Agregado para mostrar datos

--- EvaluacionGitHup/Program.cs
+++ EvaluacionGitHup/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Proyecto de ventas");
+            MostrarDatos();
         }
+        static void MostrarDatos()
+        {
+            Console.WriteLine("Nombre: Jamie Maryeli Herrera Herrera");
+            Console.WriteLine("Carnet: 24510609");
+        }
+
     }
 }
